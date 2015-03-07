1a32846e4272b2e1df2cb3af146a08ce7da1472f
Nitin Patel <nitinpatel1117@yahoo.com>
2015-03-07 14:27:41 +0000

    Re-adding the API and starting work for the dummy service

--- src/Kryptos/ServiceBundle/Controller/ConvertController.php
+++ src/Kryptos/ServiceBundle/Controller/ConvertController.php
@@ -3,6 +3,7 @@ namespace Kryptos\ServiceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Kryptos\ServiceBundle\Lib\ConversionResult;
 use Kryptos\KryptosBundle\Lib\BbanCountryMappings\Mappings;
 use Kryptos\KryptosBundle\Controller\LocaleInterface;
@@ -17,12 +18,21 @@ class ConvertController extends DefaultController implements LocaleInterface
     {
     	$this->get('locale_switcher')->setLocale($_locale);
     	
-    	$userManager = $this->get('user_manager');
+    	# $userManager = $this->get('user_manager');
     	
     	$errorExists = false;
     	$accountValid = false;
     	
-    	$user = $userManager->getUserByPublicKey($publicKey);
+    	/*
+    	try {
+    		$user = $userManager->getUserByPublicKey($publicKey);
+    	}
+    	catch (\Exception $e) {
+    		if ('Invalid object ID' == $e->getMessage()) {
+    			$user = null;
+    		}
+    	}*/
+    	$user = $this->retrieveUser($publicKey);
     	if (is_null($user)) {
     		$errorExists = true;
     		return $this->dieImmediately($this->get('translator')->trans('msg_title_invalid_credentials'), 404);
@@ -82,12 +92,22 @@ class ConvertController extends DefaultController implements LocaleInterface
     {
     	$this->get('locale_switcher')->setLocale($_locale);
     	
-    	$userManager = $this->get('user_manager');
+    	#$userManager = $this->get('user_manager');
     	 
     	$errorExists = false;
     	$accountValid = false;
     	
-    	$user = $userManager->getUserByPublicKey($publicKey);
+    	/*
+    	try {
+    		$user = $userManager->getUserByPublicKey($publicKey);
+    	}
+    	catch (\Exception $e) {
+    		if ('Invalid object ID' == $e->getMessage()) {
+    			$user = null;
+    		}
+    	}*/
+    	
+    	$user = $this->retrieveUser($publicKey);
     	if (is_null($user)) {
     		$errorExists = true;
     		return $this->dieImmediately($this->get('translator')->trans('msg_title_invalid_credentials'), 404);
@@ -168,6 +188,167 @@ class ConvertController extends DefaultController implements LocaleInterface
     	
     	$result = new ConversionResult($resultsConversion, $this->get('translator'));
     	return $this->getJsonResponse($result->toArray(), false);
+    }
+    
+    
+    public function ibanDummyAction(Request $request, $_locale, $publicKey, $iban)
+    {
+    	$user = $this->retrieveUser($publicKey);
+    	if (is_null($user)) {
+    		$errorExists = true;
+    		return $this->dieImmediately($this->get('translator')->trans('msg_title_invalid_credentials'), 404);
+    	}
+    	
+    	
+    	/* converts
+    	 *  /dummy_services/en/convert/iban/521a53c1632ed4931100000a/GB71NAIA07011621132249
+    	 *  to
+    	 * __dummy_services__en__convert__iban__publicKey__GB71NAIA07011621132249
+    	 */
+    	$search = array (
+    		'/app.php',
+    		'/nitin_dev.php',
+    		'/',
+    		$publicKey,
+    	);
+    	$replace = array(
+    		'',
+    		'',
+    		'__',
+    		'publicKey',
+    	);
+    	$requestFile = str_replace($search, $replace, $request->getRequestUri());
+    	
+    	
+    	// make filepath for dummy file
+    	$configManager = $this->get('config_manager');
+    	$dummyPath = $configManager->get('dummy_service|filepath');
+    	$dummyFilepath = sprintf('%s/%s.json', $dummyPath, $requestFile);
+    	
+    	// check file system for file
+    	if (!(file_exists($dummyFilepath) && is_readable($dummyFilepath))) {
+    		die('dummy service is not available for this API endpoint');
+    		// get translation for below
+    		return $this->dieImmediately($this->get('translator')->trans('msg_desc_required_field_iban'), 404);
+    		
+    	}
+   
+    	$data = file_get_contents($dummyFilepath);
+    	
+    	
+    	$response = new Response();
+    	$response->setContent($data);
+    	// $this->getResponse()->setStatusCode($this->getApiResponse()->code);
+    	$response->headers->set('Content-Type', 'application/json');
+    	
+    	return $response;
+    }
+    
+    
+    
+    public function bbanDummyAction(Request $request, $_locale, $publicKey, $country, $bban1, $bban2, $bban3, $bban4)
+    {
+    	$user = $this->retrieveUser($publicKey);
+    	if (is_null($user)) {
+    		$errorExists = true;
+    		return $this->dieImmediately($this->get('translator')->trans('msg_title_invalid_credentials'), 404);
+    	}
+    	$this->setupUser($user);
+    	 
+    	if (is_null($country) || ''== $country) {
+    		return $this->dieImmediately($this->get('translator')->trans('msg_desc_required_field_country_code'), 404);
+    	}
+    	$country = strtoupper($country);
+    	 
+    
+    	// check that all the bban fields for the supplied country code are provided
+    	$mappings = new Mappings();
+    	$bbanMaps = $mappings->getBbanMappings($country);
+    	$bbanOptional = $mappings->getBbanMappingsOptional($country);
+    	 
+    	if (is_array($bbanMaps)) {
+    		foreach ($bbanMaps as $key => $value) {
+    			if (!in_array($key, $bbanOptional))
+    			{
+    				if (!(isset($$key) && !empty($$key))) {
+    					$errorExists = true;
+    					$msg = sprintf('%s. %s', $this->get('translator')->trans('msg_title_invalid_details'), $this->get('translator')->trans('msg_desc_bban_required', array('{{ bban_fieldname }}' => $value)));
+    					return $this->dieImmediately($msg, 404);
+    				}
+    			}
+    		}
+    	}
+    
+    
+    	// check that the user has some credits
+    	if ($this->conversionsRestricted()) {
+    		$credits = $this->getAllowedConversions();
+    		if ($credits < 1) {
+    			$errorExists = true;
+    			$msg = sprintf('%s %s', $this->get('translator')->trans('msg_title_insufficient_credit'), $this->get('translator')->trans('msg_desc_insufficient_credit'));
+    			return $this->dieImmediately($msg);
+    		}
+    	}
+    
+    
+    	if (false == $errorExists)
+    	{
+    		$chargeUser = false;
+    		$resultsConversion = $this->get('single_conversion');
+    
+    		// build our commands
+    		$args = array();
+    		if (is_array($bbanMaps)) {
+    			foreach ($bbanMaps as $key => $value) {
+    				$args[] = $$key;
+    			}
+    
+    			$resultsConversion->runCountry($country, $args);
+    		}
+    
+    
+    		if (true == $resultsConversion->isFatal) {
+    			$msg = sprintf('%s %s', $this->get('translator')->trans('msg_title_conversion_tool_offline'), $this->get('translator')->trans('msg_desc_conversion_tool_offline'));
+    			$this->getApiResponse()->error($msg, 404);
+    		} else if (false == $resultsConversion->isValid) {
+    			$this->getApiResponse()->error($resultsConversion->getErrorsAsString(), 404);
+    			$chargeUser = true;
+    		}
+    		else {
+    			$accountValid = true;
+    			$chargeUser = true;
+    		}
+    
+    		if (true == $chargeUser) {
+    			if ($this->conversionsRestricted()) {
+    				$user = $this->get('user_manager');
+    				$user->reduceCredit($this->getUserId(), 'singleApi');
+    			}
+    		}
+    	}
+    	 
+    	$result = new ConversionResult($resultsConversion, $this->get('translator'));
+    	return $this->getJsonResponse($result->toArray(), false);
+    }
+    
+    
+    /**
+     * 
+     */
+    protected function retrieveUser($publicKey)
+    {
+    	$userManager = $this->get('user_manager');
+    	
+    	try {
+    		$user = $userManager->getUserByPublicKey($publicKey);
+    	}
+    	catch (\Exception $e) {
+    		if ('Invalid object ID' == $e->getMessage()) {
+    			$user = null;
+    		}
+    	}
+    	
+    	return $user;
     }
     
     
